529592b23b662ac203a5bcab69be5a8027b7dfc4
you777777 <691nbt80@gmail.com>
2025-04-25 13:20:24 +0900

    エラーメッセージの表示機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
-  validates :first_name, presence: true,  length: { maximum: 100 }
   validates :last_name, presence: true,  length: { maximum: 100 }
+  validates :first_name, presence: true,  length: { maximum: 100 }
   validates :email, presence: true, uniqueness: true
   validates :password,  length: { in: 6..20 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
