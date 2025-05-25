1883d8d93e681ef1eda2f4a8259c9c920e5d9eea
you777777 <691nbt80@gmail.com>
2025-05-25 10:33:55 +0900

    データ分析機能の実装

--- app/models/noise_report.rb
+++ app/models/noise_report.rb
@@ -8,10 +8,6 @@ class NoiseReport < ApplicationRecord
   enum :noise_type, { noise_type_other: 0, footstep: 1, appliance: 2, human_voice: 3,  entertainment: 4, impact: 5, pet: 6, external: 7 }
   enum :frequency, { frequency_other: 0, daily: 1, weekly: 2, few: 3, irregular: 4 }
 
-  def self.human_enum_name(enum_name, key)
-    I18n.t("enums.#{model_name.i18n_key}.#{enum_name}.#{key}")
-  end
-
   def self.ransackable_attributes(auth_object = nil)
     [ "created_at", "frequency", "id", "id_value", "location", "memo", "noise_type", "recording_id", "time_period", "title", "updated_at", "user_id" ]
   end
